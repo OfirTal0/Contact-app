f9c829f5dfae19965dae593efc1fe628e1de72af
Ofir Tal <ofirital0@gmail.com>
2024-12-05 23:42:27 +0200

    host 5/12

--- contacts/settings.py
+++ contacts/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--rrm*#of_=5p09f86fjhhq+y=xwmva64l%#(lf(#k@3+xpyny9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'contact-app-production.up.railway.app',
