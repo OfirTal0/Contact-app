747fa88e6924c1f8a60aa33931e4e93257966da1
Ofir Tal <ofirital0@gmail.com>
2025-02-24 14:49:14 +0200

    deploy fix 24.2

--- contacts/wsgi.py
+++ contacts/wsgi.py
@@ -1,20 +1,9 @@
-"""
-WSGI config for contacts project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 from django.middleware.security import SecurityMiddleware
 from django.contrib.sessions.middleware import SessionMiddleware
 
-application = SessionMiddleware(get_wsgi_application())
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contacts.settings')
 
-application = get_wsgi_application()
+application = get_wsgi_application()  # תקרא לפונקציה זו פעם אחת בלבד
+application = SessionMiddleware(application)  # הוסף את המידלוורק של session אחרי זה
